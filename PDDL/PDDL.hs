88bc6c63e5d117881711fd4acdbd0331f884c2b4
Søren Jacobsen <srnjcbsn@gmail.com>
2015-01-27 09:08:43 +0100

    used newtype instead of type in some places

--- PDDL/PDDL.hs
+++ PDDL/PDDL.hs
@@ -7,23 +7,28 @@ import Data.List (intercalate)
 type Name = String
 
 type Variable = Name
-type Fluent = (Name, [Variable])
+newtype Fluent = Fluent (Name, [Variable]) deriving (Show, Ord, Eq)
 type GroundedFluent = (Name, [Name])
 
 data Formula = Predicate Fluent
              | Neg Formula
              | Con (Set Formula)
-             deriving (Ord, Eq)
+             deriving (Ord, Eq, Show)
 
-type ActionSpec = (Name, [Name], Formula, Formula)
+data ActionSpec = ActionSpec
+    { name :: String
+    , variables :: [Variable]
+    , precond :: Formula
+    , effect :: Formula
+    } deriving Show
 
 type Action = (Name, [Name])
 
 data Domain = Domain
     { predicates   :: Set Fluent
-    , actionsSpecs :: Set ActionSpec
+    , actionsSpecs :: [ActionSpec]
     , constants    :: Set Name
-    }
+    } deriving Show
 
 type State = Set GroundedFluent
 
@@ -34,20 +39,25 @@ data Problem = Problem
 
 dom :: Domain
 dom =
-    let preds = Set.fromList [ ("at", ["l"])
-                             , ("clean", ["l"])
+    Domain { predicates = preds
+    , actionsSpecs = [suck]
+    , constants = Set.empty
+    }
+    where
+        preds = Set.fromList [ Fluent ("at", ["l"])
+                             , Fluent ("clean", ["l"])
                              ]
 
-        precs l = Con (Set.fromList [ Predicate ("at", [l])
-                                    , Neg (Predicate ("clean", [l]))
+        precs l = Con (Set.fromList [ Predicate $ Fluent ("at", [l])
+                                    , Neg (Predicate $ Fluent ("clean", [l]))
                                     ])
 
-        effects l = Predicate ("clean", [l])
-        suck = ("suck", ["l"], precs "l", effects "l")
-      in Domain { predicates = preds
-                , actionsSpecs = Set.fromList [suck]
-                , constants = Set.empty
-                }
+        effects l = Predicate $ Fluent ("clean", [l])
+        suck = ActionSpec { name = "suck"
+                          , variables = ["l"]
+                          , precond = precs "l"
+                          , effect = effects "l"
+                          }
 
 type Planner = Domain -> Problem -> Maybe [Action]
 
@@ -63,29 +73,39 @@ showParameters ls = unwords $ map ("?" ++) ls
 class PDDL p where
     toPddl :: p -> String
 
-instance PDDL Formula where
-    toPddl (Predicate (n, vs)) =
-        "(" ++ n ++ " " ++ showParameters vs ++ ")"
-    toPddl (Neg f) = "(not " ++ toPddl f ++ ")"
-    toPddl (Con fs) = "(and " ++ unwords (map toPddl (Set.toList fs)) ++ ")"
+-- instance PDDL ActionSpec where
+--     toPDDL as =
+--         "(" ++ name as
+--         ++ "\t:parameters (" ++ showParameters (variables as) ++ ")\n"
+--         ++ "\t:precondition (" ++ toPddl (precond as) ++ ")\n"
+--         ++ "\t:effect (" ++ toPddl (effect as) ++ ")\n"
+--         ++ ")"
 
-instance PDDL Domain where
-    toPddl domain =
-        let consts = Set.toList (constants domain)
-            constsStr = "(:constants" ++ unwords consts ++ ")"
-            preds = Set.toList (predicates domain)
-            showFluent (n, vs) = "(" ++ n ++ " " ++ showParameters vs ++ ")"
-            predsStr = "(:predicates " ++ unwords (map showFluent preds) ++ ")"
-            showActionS (n, vs, p, e) =
-                "(:action " ++ n ++ "\n\t"
-                ++ ":parameters (" ++ showParameters vs ++ ")\n\t"
-                ++ ":precondition (" ++ toPddl p ++ ")\n\t"
-                ++ ":effect (" ++ toPddl e ++ ")\n\t"
-                ++ ")"
-            actions = unwords $ map showActionS $ (Set.toList . actionsSpecs) domain
-        in intercalate "\n\t" [constsStr, predsStr, actions]
-
-domainToPdll :: String -> Domain -> String
-domainToPdll name domain =
-    "(define (domain " ++ name ++ ")\n\t(:requirements :strips)\n\t"
-    ++ toPddl domain ++ "\n\t)"
+-- instance PDDL Fluent where
+--     toPddl (Fluent (name, vs)) =
+--         "(" ++ name ++ " " ++ showParameters vs ++ ")"
+--
+-- instance PDDL Formula where
+--     toPddl (Predicate f) = toPddl f
+--     toPddl (Neg f) = "(not " ++ toPddl f ++ ")"
+--     toPddl (Con fs) = "(and " ++ unwords (map toPddl (Set.toList fs)) ++ ")"
+--
+-- instance PDDL Domain where
+--     toPddl domain =
+--         let consts = Set.toList (constants domain)
+--             constsStr = "(:constants" ++ unwords consts ++ ")"
+--             preds = Set.toList (predicates domain)
+--             predsStr = "(:predicates " ++ unwords (map toPddl preds) ++ ")"
+--             showActionS (n, vs, p, e) =
+--                 "(:action " ++ n ++ "\n\t"
+--                 ++ ":parameters (" ++ showParameters vs ++ ")\n\t"
+--                 ++ ":precondition (" ++ toPddl p ++ ")\n\t"
+--                 ++ ":effect (" ++ toPddl e ++ ")\n\t"
+--                 ++ ")"
+--             actions = unwords $ map showActionS $ (Set.toList . actionsSpecs) domain
+--         in intercalate "\n\t" [constsStr, predsStr, actions]
+--
+-- domainToPdll :: String -> Domain -> String
+-- domainToPdll name domain =
+--     "(define (domain " ++ name ++ ")\n\t(:requirements :strips)\n\t"
+--     ++ toPddl domain ++ "\n\t)"
