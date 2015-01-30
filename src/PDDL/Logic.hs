a44a06d84f20328b2129e9b383af203ad516f6d0
jannick johnsen <jannickboese@msn.com>
2015-01-30 18:00:57 +0100

    added more unit tests for logic * testing edge cases and failures

--- src/PDDL/Logic.hs
+++ src/PDDL/Logic.hs
@@ -1,4 +1,4 @@
-module PDDL.Logic where
+module PDDL.Logic (isActionValid, apply, findActionSpec, unionChanges) where
 
 import PDDL.Type
 import qualified Data.List as List
@@ -19,19 +19,19 @@ findActionSpec domain action = actionsSpec
 unionChanges :: GroundedChanges -> GroundedChanges -> GroundedChanges
 unionChanges (pos,neg) (pos2,neg2) = (Set.union pos pos2, Set.union neg neg2)
 
-instantiateFormula :: Map Argument Object -> Formula -> GroundedChanges
-instantiateFormula m (Predicate p) = (Set.singleton (pName p, List.map (m Map.!) $ pArgs p), Set.empty)
-instantiateFormula m (Neg f) = swap $ instantiateFormula m f
-instantiateFormula m (Con fs) = List.foldl (\changes f -> unionChanges changes $ instantiateFormula m f ) (Set.empty,Set.empty) fs
+instantiateFormula :: State -> Map Argument Object -> Formula -> GroundedChanges
+instantiateFormula s m (Predicate p) = (Set.singleton (pName p, List.map (m Map.!) $ pArgs p), Set.empty)
+instantiateFormula s m (Neg f) = swap $ instantiateFormula s m f
+instantiateFormula s m (Con fs) = List.foldl (\changes f -> unionChanges changes $ instantiateFormula s m f ) (Set.empty,Set.empty) fs
 
 
-instantiateAction :: Map Argument Object -> ActionSpec -> Action -> GroundedAction
-instantiateAction m as act = ga
+instantiateAction :: State -> Map Argument Object -> ActionSpec -> Action -> GroundedAction
+instantiateAction s m as act = ga
   where
     pairs = List.zip (List.map Ref $ asParas as) (aArgs act)
     paraMap = Map.fromAscList pairs
     fullMap = Map.union paraMap m
-    ga = (instantiateFormula fullMap (asPrecond as), instantiateFormula fullMap (asEffect as))
+    ga = (instantiateFormula s fullMap (asPrecond as), instantiateFormula s fullMap (asEffect as))
 
 isActionValid :: State -> GroundedAction -> Bool
 isActionValid s ((posCond,negCond),(posEff,negEff)) =
@@ -52,8 +52,10 @@ apply domain curState action = newState
     newState = case findActionSpec domain action of
                 Just actSpec -> out
                   where
-                    gact@(_,(pos,neg)) = instantiateAction mapDomain actSpec action
-                    out = if Set.null (Set.intersection pos neg)
+                    gact@(_,(pos,neg)) = instantiateAction curState mapDomain actSpec action
+                    ambiguousEffects = Set.intersection pos neg
+                    out = if Set.null ambiguousEffects
                           then applyAction curState gact
-                          else error ("ambiguous effects in action " ++ aName action)
+                          else error $ "ambiguous effects in " ++ aName action ++ ": " ++ show (Set.toList ambiguousEffects)
+
                 Nothing      -> Nothing
--- testsuite/PDDL/LogicSpec.hs
+++ testsuite/PDDL/LogicSpec.hs
@@ -7,45 +7,88 @@ import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 import qualified Data.Set  as Set
 import           Data.Set  (Set)
+import Control.Exception
 
-testObj = "testObj1"
 
-actionInp = ("testAction", [testObj])
 
-getPred id = Predicate ("testPred" ++ show id, [Ref "x"])
+
+getAction obj = ("testAction", [obj])
+
+getState :: [GroundedPredicate] -> State
+getState = Set.fromList
+
+getPred = Predicate ("testPred1", [Ref "x"])
 
 getActSpec conds effects  = ActionSpec { asName = "testAction"
                          , asParas = ["x"]
                          , asPrecond = Con conds
                          , asEffect = Con effects
                          }
-getGroundPred id objs = ("testPred" ++ show id, objs)
+getGroundPred objs = ("testPred1", objs)
+
+
+getGroundAct posCond negCond posEff negEff = ((getState posCond,getState negCond),(getState posEff,getState negEff))
 
 getDomain as = Domain { dmPredicates = []
                     , dmActionsSpecs = as
                     , dmConstants = []
                     }
 
-getState :: [GroundedPredicate] -> State
-getState = Set.fromList
+emptyState = getState []
+testObj = "testObj1"
+actionCondionA = getPred
+actionEffectA = getPred
 
 testLogicSpec :: Spec
 testLogicSpec = do
     describe "Apply" $ do
-      it "can add effects to state" $ do
-        let initState = getState []
-            actSpec = getActSpec [] [getPred 1]
+      it "can add effects to state" $
+        let actSpec = getActSpec [] [actionEffectA]
             domain = getDomain [actSpec]
-            expectedState = getState [getGroundPred 1 [testObj]] in
-          apply domain initState actionInp `shouldBe` Just expectedState
+            action = getAction testObj
+            expectedState = getState [getGroundPred [testObj]] in
+          apply domain emptyState action `shouldBe` Just expectedState
 
-      it "can remove effects to state" $ do
-        let initState = getState [getGroundPred 1 [testObj]]
-            expectedState = getState []
-            actSpec = getActSpec [] [Neg $ getPred 1]
-            domain = getDomain [actSpec] in
-          apply domain initState actionInp `shouldBe` Just expectedState
+      it "can remove effects to state" $
+        let initState = getState [getGroundPred [testObj]]
+            actSpec = getActSpec [] [Neg $ actionEffectA]
+            domain = getDomain [actSpec]
+            action = getAction testObj in
+          apply domain initState action `shouldBe` Just emptyState
 
+      it "fails on action with ambiguous effects" $
+        let truePred = getGroundPred [testObj]
+            initState = getState [truePred]
+            actSpec = getActSpec [] [Neg $ actionEffectA, actionEffectA] -- same predicate for pos and neg
+            domain = getDomain [actSpec]
+            action = getAction testObj in
+          evaluate (apply domain initState action) `shouldThrow` errorCall ("ambiguous effects in " ++ aName action ++ ": " ++ show [truePred])
+
+      it "gives nothing when the action is invalid" $
+        let actSpec = getActSpec [actionCondionA] []
+            domain = getDomain [actSpec]
+            action = getAction testObj in
+          apply domain emptyState action `shouldBe` Nothing
+
+
+    describe "isActionValid" $ do
+      it "can check if a positive precondition is in the state" $
+        let initState = getState [getGroundPred [testObj]]
+            groundAct = getGroundAct [getGroundPred [testObj]] [] [] [] in
+          isActionValid initState groundAct `shouldBe` True
+
+      it "can check if a positive precondition is not in the state" $
+        let groundAct = getGroundAct [getGroundPred [testObj]] [] [] [] in
+          isActionValid emptyState groundAct `shouldBe` False
+
+      it "can check if a negative precondition is in the state" $
+        let initState = getState [getGroundPred [testObj]]
+            groundAct = getGroundAct [] [getGroundPred [testObj]] [] [] in
+          isActionValid initState groundAct `shouldBe` False
+
+      it "can check if a negative precondition is not in the state" $
+        let groundAct = getGroundAct [] [getGroundPred [testObj]] [] [] in
+          isActionValid emptyState groundAct `shouldBe` True
 spec :: Spec
 spec = testLogicSpec
 
