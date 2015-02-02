7778aa40d0a9c817febbc388c1a0e7342f88627e
Jannick Johnsen <jannickboese@msn.com>
2015-02-02 17:15:12 +0100

    begun work on deduction

--- src/Learning/Deduction.hs
+++ src/Learning/Deduction.hs
@@ -2,6 +2,29 @@ module Deduction where
 
 import PDDL.Type
 import qualified Data.Set as Set
+import              Data.Set (Set)
+import qualified Data.List as List
+import qualified Data.Map as Map
+import              Data.Map (Map)
 
-unground :: ActionSpec -> [Name] -> Action -> Set FluentPredicate
-unground = undefined
+
+
+mapfluents :: (Map Object [Name]) -> [Object] -> Set [Argument]
+mapfluents _ [] = Set.fromList []
+mapfluents mapper (obj:rest) = Set.fromList []
+  where
+    restMapped = mapfluents mapper rest
+    f o =
+      case Map.lookup o mapper of
+        Just names -> outp where
+          p n = Set.map ((:) $ Ref n) restMapped
+          allOfThem = List.map p names
+          outp = List.foldr (Set.union) Set.empty allOfThem
+        Nothing ->  Set.empty -- [Const o]
+
+
+unground :: ActionSpec -> [Name] -> GroundedPredicate -> Action -> [FluentPredicate]
+unground as consts gp (_, objs) = undefined
+  where
+    paras = asParas as
+    objsMap = Map.fromAscListWith (++) $ List.zip objs paras
