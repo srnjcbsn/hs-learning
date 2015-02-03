3a1555cb7e04d5338fc28579db31be7d6869feef
jannick johnsen <jannickboese@msn.com>
2015-02-03 14:47:09 +0100

    Made tests easier to read

--- testsuite/Learning/DeductionSpec.hs
+++ testsuite/Learning/DeductionSpec.hs
@@ -9,7 +9,7 @@ import qualified Data.Set  as Set
 import           Data.Set  (Set)
 import qualified Data.List as List
 
-paras = ["x", "y"]
+
 getArgs = List.map snd
 getNames = List.map fst
 
@@ -17,31 +17,32 @@ testLogicSpec :: Spec
 testLogicSpec =
     describe "deduct" $ do
       it "can deduct all parameter posibilities when two arguments are the same" $
-        let actArgs = ["1", "1"]
-            groundPref = ("testPred", ["1", "1"])
+        let paras = ["x", "y"]
+            actArgs = ["1", "1"]
+            groundPref = ("_", ["1", "1"])
             expected = Set.fromList [ [Ref "x", Ref "y"], [Ref "y", Ref "x"], [Ref "x", Ref "x"], [Ref "y", Ref "y"] ]
             actual = Set.fromList $ getArgs $ deduct paras actArgs groundPref   in
             actual `shouldBe` expected
 
       it "can deduct the exact parameters when two arguments are different" $
-        let actArgs = ["1", "2"]
-            groundPref = ("testPred", ["1", "2"])
+        let paras = ["x", "y"]
+            actArgs = ["1", "2"]
+            groundPref = ("_", ["1", "2"])
             expected = [ [Ref "x", Ref "y"] ]
             actual = getArgs $ deduct paras actArgs groundPref   in
             actual `shouldBe` expected
 
       it "can deduct constants" $
-        let actArgs = ["_"]
-            groundPref = ("testPred", ["_", "A"])
-            expected = [ [Ref "x", Const "A"] ]
-            actual = getArgs $ deduct paras actArgs groundPref   in
+        let groundPref = ("_", ["A"])
+            expected = [ [Const "A"] ]
+            actual = getArgs $ deduct [] [] groundPref   in
             actual `shouldBe` expected
 
       it "provides fluents with same name" $
-        let actArgs = ["_"]
-            groundPref = ("testPred", ["_"])
-            expected = [ "testPred" ]
-            actual = getNames $ deduct paras actArgs groundPref   in
+        let predName = "testPred"
+            groundPref = (predName, [])
+            expected = [ predName ]
+            actual = getNames $ deduct [] [] groundPref   in
             actual `shouldBe` expected
 
 spec :: Spec
