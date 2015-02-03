3df431389b2647b88dd5e25e74fe5afd0da3a017
jannick johnsen <jannickboese@msn.com>
2015-02-03 23:38:26 +0100

    updated deduction ** Simplified now returns ([x,y],z) instead of (x,z) and (y,z) ** Added functionality to turn deductions into fluents ** reworked unit tests for deductions and added new for turning them into pddl and for collecting them

--- src/Learning/Deduction.hs
+++ src/Learning/Deduction.hs
@@ -1,11 +1,11 @@
-module Learning.Deduction where
+module Learning.Deduction(expandFluents, deduct, collectDeducts, collectManyDeduct, variants, asPDDL, unground) where
 
-import PDDL.Type
-import qualified Data.Set as Set
-import           Data.Set (Set)
 import qualified Data.List as List
-import qualified Data.Map as Map
-import           Data.Map (Map)
+import           Data.Map  (Map)
+import qualified Data.Map  as Map
+import           Data.Set  (Set)
+import qualified Data.Set  as Set
+import           PDDL.Type
 
 appendToAll :: [[a]] -> a -> [[a]]
 appendToAll ll ele = List.map ((:) ele)  ll
@@ -13,26 +13,55 @@ appendToAll ll ele = List.map ((:) ele)  ll
 listify :: [a] -> [[a]]
 listify = List.map (:[])
 
+
 mapMany :: Ord a => [a] -> [b] -> Map a [b]
 mapMany a b = Map.fromAscListWith (++) $ List.zip a (listify b)
 
-fluentVariants :: Map Object [Name] -> [Object] -> [ [Argument] ]
-fluentVariants _ [] = [[]]
-fluentVariants mapper (obj:rest) = allmapped
-  where
-    restMapped = fluentVariants mapper rest
-    allmapped =
-       case Map.lookup obj mapper of
-        Just names -> List.concatMap (appendToAll restMapped . Ref) names
-        Nothing -> appendToAll restMapped $ Const obj
+eitherLookup :: (Ord a, Ord b) => Map a [b] -> a -> Set (Either a b)
+eitherLookup mapAB a =
+  case Map.lookup a mapAB of
+    Just b -> Set.fromList $ List.map Right b
+    Nothing -> Set.singleton $ Left a
 
-deduct :: Parameters-> Arguments -> GroundedPredicate -> [FluentPredicate]
-deduct paras args (name, predObjs) = collect
+deduct :: (Ord arg, Ord para) => [para] -> [arg] -> [arg] -> [Set (Either arg para)]
+deduct paras args res =  deduction
   where
     objsMap = mapMany args paras
-    toFluent objs = (name, objs)
-    variants = fluentVariants objsMap predObjs
-    collect = List.map toFluent variants
+    deduction = List.map (eitherLookup objsMap) res
 
-deductFromAction :: ActionSpec -> Action -> GroundedPredicate -> [FluentPredicate]
-deductFromAction as (_, args) = deduct (asParas as) args
+collectDeducts :: Ord a => [ Set a ] -> [Set a] -> [Set a]
+collectDeducts d1 d2 = combined
+  where
+    combineLists (l1,l2) = Set.intersection l1 l2
+    zipped = List.zip d1 d2
+    combined = List.map combineLists zipped
+
+collectManyDeduct :: Ord a => [ [ Set a ] ] -> [ Set a  ]
+collectManyDeduct = List.foldl1 collectDeducts
+
+variants :: [ Set a ] -> [ [a] ]
+variants [] = [ [] ]
+variants (args:rest) = allmapped
+  where
+    restMapped = variants rest
+    allmapped = List.concatMap (appendToAll restMapped) (Set.toList args)
+
+asPDDL :: [Set (Either Object Name)] -> [Set Argument]
+asPDDL res = pddl
+  where
+    toPDDL v =
+      case v of
+       Left arg -> Const arg
+       Right p -> Ref p
+    pddl = List.map (Set.map toPDDL) res
+
+unground :: Parameters -> Arguments -> [Object] -> [Set Argument]
+unground paras args objs = deduction
+  where
+    deduction = asPDDL $ deduct paras args objs
+
+expandFluents :: [Set Argument] -> Name -> [FluentPredicate]
+expandFluents argOptions name = preds
+  where
+    predArgs = variants argOptions
+    preds = List.map (\x -> (name, x)) predArgs
