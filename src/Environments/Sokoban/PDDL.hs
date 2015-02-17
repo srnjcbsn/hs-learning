f0f35934d1fc1a832918fa1ab07e428c9f47e4ec
Jannick Johnsen <jannickboese@msn.com>
2015-02-17 15:31:09 +0100

    Added action for move on sokoban

--- src/Environments/Sokoban/PDDL.hs
+++ src/Environments/Sokoban/PDDL.hs
@@ -4,7 +4,7 @@ import Environments.Sokoban.Sokoban hiding (Object)
 import PDDL.Type
 
 import qualified Data.Map as Map
-import Data.Map (Map)
+import Data.Map (Map,(!))
 
 data SokobanPDDL = SokobanPDDL
     { world  :: World
@@ -14,12 +14,6 @@ data SokobanPDDL = SokobanPDDL
 type Location = Object
 type Crate = Object
 
-movH = "move-h"
-movV = "move-v"
-pushH = "push-h"
-pushV = "push-v"
-pushHGoal = "push-h-goal"
-pushVGoal = "push-v-goal"
 
 hAdj :: Location -> Location -> GroundedPredicate
 hAdj from to = ("hAdj", [from, to])
@@ -45,8 +39,26 @@ goal loc = ("goal", [loc])
 notGoal :: Location -> GroundedPredicate
 notGoal loc = ("notGoal", [loc])
 
-applyAction :: SokobanPDDL -> GroundedAction -> SokobanPDDL
-applyAction = undefined
+directionFromObjs :: SokobanPDDL -> Location -> Location -> Direction
+directionFromObjs pddlw from to =
+  let fromPos = objMap pddlw ! from
+      toPos = objMap pddlw ! to
+   in case (toPos - fromPos) of
+        Coord (0, 1) -> UpDir
+        Coord (0, -1) -> DownDir
+        Coord (1, 0) -> RightDir
+        Coord (-1, 0) -> LeftDir
+        _ -> error ("cannot get direction (fromPos: " ++ show fromPos ++", toPos: " ++ show toPos ++ ")")
+
+
+applyAction :: SokobanPDDL -> Action -> SokobanPDDL
+applyAction pddlw ("move-h", [from, to]) = pddlw { world = move (world pddlw) $ directionFromObjs pddlw from to}
+applyAction pddlw ("move-v", args) = undefined
+applyAction pddlw ("push-h", args) = undefined
+applyAction pddlw ("push-v", args) = undefined
+applyAction pddlw ("push-h-goal", args) = undefined
+applyAction pddlw ("push-v-goal", args) = undefined
+applyAction _ act = error ("Unknown action: " ++ show act)
 
 fromState :: State -> SokobanPDDL
 fromState = undefined
--- src/Environments/Sokoban/Sokoban.hs
+++ src/Environments/Sokoban/Sokoban.hs
@@ -8,17 +8,50 @@ data Tile = Clear
 
 type Object = String
 
-type Coord = (Int, Int)
+data Coord = Coord (Integer, Integer)
+             deriving (Ord,Eq,Show)
+
+instance Num Coord where
+  (+) (Coord (x1,y1)) (Coord (x2,y2)) = Coord (x1+x2, y1+y2)
+  (*) (Coord (x1,y1)) (Coord (x2,y2)) = Coord (x1+x2, y1+y2)
+  abs (Coord (x,y)) = Coord (abs x, abs y)
+  signum (Coord (x,y)) = Coord (signum x, signum y)
+  fromInteger i = Coord (i, i)
+  (-) (Coord (x1,y1)) (Coord (x2,y2)) = Coord (x1-x2, y1-y2)
+
 
 data World = World
-    { coordMap :: Map Coord Tile
+    { coordMap :: Map Coord  Tile
     , sokoban  :: Coord
     }
 
-data Direction = Left
-               | Right
-               | Up
-               | Down
+data Direction = LeftDir
+               | RightDir
+               | UpDir
+               | DownDir
+
+moveCreate :: Coord -> Coord -> Map Coord Tile -> Map Coord Tile
+moveCreate from to cMap =
+  Map.insert to Box (Map.insert from Clear cMap)
+
+moveVector :: World -> Coord -> World
+moveVector world vec  =
+  let sokoPos = sokoban world
+      moveTo = vec + sokoPos
+      pushTo = (vec*2) + sokoPos
+      atTo = Map.lookup moveTo $ coordMap world
+      atPushTo = Map.lookup pushTo $ coordMap world
+  in case (atTo, atPushTo) of
+       (Just Clear, _) -> world { sokoban = moveTo }
+       (Just Box, Just Clear) ->
+        world { sokoban = moveTo
+              , coordMap = moveCreate moveTo pushTo (coordMap world)
+              }
+       (Nothing, _) -> world
+       (Just Box, _) -> world
 
 move :: World -> Direction -> World
-move = undefined
+move w UpDir = moveVector w (Coord (0,1))
+move w DownDir = moveVector w (Coord (0,-1))
+move w LeftDir = moveVector w (Coord (-1,0))
+move w RightDir = moveVector w (Coord (1,0))
