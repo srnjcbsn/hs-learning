ae43d98f1b330b45941d89e5fb66e3f45592454f
Jannick Johnsen <jannickboese@msn.com>
2015-04-06 16:18:43 +0200

    Fixed some stuff

--- src/Learning2.hs
+++ src/Learning2.hs
@@ -13,35 +13,46 @@ import qualified Learning.SchemaLearning as Lrn
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-class Knowledge knl info question where
+class Knowledge knl info question | knl -> info question where
     analyze :: knl -> info -> knl
     canAnswer :: knl -> question -> Bool
 
-class Experiment exp world info where
-    conduct :: exp -> world -> IO info
+class Experiment exp world info | exp -> world info  where
+    conduct :: exp -> world -> IO (info, world)
 
-class (Experiment exp world info) => Strategy world knl exp info where
+class Experiment exp world info => Strategy strat world knl exp info | strat -> exp knl where
     design :: strat -> knl -> exp
 
 
-class Inquirable uni question info where
+class Inquirable uni question info | question -> info where
     inquire :: uni -> question -> IO (Maybe info)
 
 
-answered = undefined
-
-scientificMethod :: ( Strategy world knl exp info
+scientificMethod :: ( Strategy strat world knl exp info
                     , Experiment exp world info
-                    , Inquirable world question info)
-                 => world -> strat -> knl -> question -> IO (knl)
+                    , Inquirable world question info
+                    , Knowledge knl info question )
+                 => world -> strat -> knl -> question -> IO knl
 scientificMethod world strat knowledge question  =
   do information <- inquire world question
-     let knowledge' = liftM (analyze knowledge) information
-     let loop knl =
-          do experiment <- design strat knl
-             testdata <- conduct world experiment
+     let knowledge' = fromMaybe knowledge (liftM (knowledge `analyze`)  information )  -- undefined --liftM (analyze knowledge) information
+     let loop w knl =
+          do let experiment = design strat knl
+             (testdata, w') <- conduct experiment w
              let knl' = analyze knl testdata
-             if answered knl' question
-             then knl'
-             else loop knl'
-      in loop >> knowledge'
+             if canAnswer knl' question
+             then return knl'
+              else loop w' knl'
+      in loop world knowledge'
+
+
+
+
+
+
+
+
+
+
+
+      ---fuck søren
