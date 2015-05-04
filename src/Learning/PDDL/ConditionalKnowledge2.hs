508faa7dfd0b169d685dead2fc7426f2b6e02097
Søren Jacobsen <srnjcbsn@gmail.com>
2015-05-04 13:53:04 +0200

    Added temporary file for changes

---
+++ src/Learning/PDDL/ConditionalKnowledge2.hs
@@ -0,0 +1,58 @@
+module Learning.PDDL.ConditionalKnowledge2 where
+
+import           Data.TupleSet
+import           Data.Typeable
+import           Environment
+import qualified Learning                            as Lrn
+import qualified Learning.Induction                  as Ind
+import qualified Learning.PDDL                       as PDDL
+import qualified Learning.PDDL.EffectKnowledge       as Eff
+import qualified Learning.PDDL.PreconditionKnowledge as Pre
+import           Logic.Formula
+import           Planning
+import           Planning.PDDL
+
+import           Control.Monad                       (sequence, replicateM)
+import           Data.Map                            (Map)
+import qualified Data.Map                            as Map
+import           Data.Maybe
+import           Data.Set                            (Set, (\\))
+import qualified Data.Set                            as Set
+import           Data.Tree
+import           Data.TupleSet                       (TupleSet)
+import qualified Data.TupleSet                       as TSet
+
+type DomainKnowledge = Map Name Pattern
+
+data Binding = Bound Int
+             | Free  Int
+
+type Knowledge = TupleSet (Predicate Binding)
+
+data Theory = Theory
+    { known   :: Knowledge
+    , unknown :: Knowledge
+    }
+
+data Cond = Cond
+    { precs     :: Theory
+    , precCands :: Set Knowledge
+    }
+
+type ActionTheory = Map (Predicate Int) Cond
+
+type Subst = Map Object Int
+
+type CArg = Int
+
+data Literal a = Pos a
+               | Neg a
+               deriving (Eq, Ord)
+
+data Pattern = Pattern
+    { ctEffArg  :: [CArg]
+    , ctPreconds :: PDDL.PreKnowledge CArg
+    } deriving (Eq, Ord)
+
+newtype ConditionalKnowledge =
+  ConditionalKnowledge (Map (Literal Name) Pattern)
