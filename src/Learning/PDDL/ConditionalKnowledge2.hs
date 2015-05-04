9ee7a66eec401bba6072ef4652b3f587dbaa9d5d
Søren Jacobsen <srnjcbsn@gmail.com>
2015-05-04 16:30:04 +0200

    New types and function stubs for conditional knowledge

--- src/Learning/PDDL/ConditionalKnowledge2.hs
+++ src/Learning/PDDL/ConditionalKnowledge2.hs
@@ -8,11 +8,12 @@ import qualified Learning.Induction                  as Ind
 import qualified Learning.PDDL                       as PDDL
 import qualified Learning.PDDL.EffectKnowledge       as Eff
 import qualified Learning.PDDL.PreconditionKnowledge as Pre
-import           Logic.Formula
+import           Logic.Formula                       hiding (Formula)
+import qualified Logic.Formula                       as LF
 import           Planning
 import           Planning.PDDL
 
-import           Control.Monad                       (sequence, replicateM)
+import           Control.Monad                       (replicateM, sequence)
 import           Data.Map                            (Map)
 import qualified Data.Map                            as Map
 import           Data.Maybe
@@ -22,10 +23,11 @@ import           Data.Tree
 import           Data.TupleSet                       (TupleSet)
 import qualified Data.TupleSet                       as TSet
 
-type DomainKnowledge = Map Name Pattern
+-- type DomainKnowledge = Map Name Pattern
 
-data Binding = Bound Int
-             | Free  Int
+data Binding = Bound CArg
+             | Free
+             deriving (Eq, Ord, Show)
 
 type Knowledge = TupleSet (Predicate Binding)
 
@@ -47,12 +49,53 @@ type CArg = Int
 
 data Literal a = Pos a
                | Neg a
-               deriving (Eq, Ord)
+               deriving (Eq, Ord, Show)
+
+type LitPred a = Literal (Predicate a)
+type Cands a = Set (Set (LitPred a))
+
+type CondPred = LitPred Binding
 
 data Pattern = Pattern
-    { ctEffArg  :: [CArg]
-    , ctPreconds :: PDDL.PreKnowledge CArg
-    } deriving (Eq, Ord)
+    { ctEffArg   :: CondPred
+    , ctPreconds :: Set CondPred
+    } deriving (Ord, Eq, Show)
 
-newtype ConditionalKnowledge =
-  ConditionalKnowledge (Map (Literal Name) Pattern)
+data ConditionalKnowledge = ConditionalKnowledge
+    { ckPattern :: Pattern
+    , ckCands   :: Cands Binding
+    , ckKnown   :: Set CondPred
+    } deriving (Ord, Eq, Show)
+
+newtype ActionKnowledge =
+    ActionKnowledge (Map (Literal Name) ConditionalKnowledge)
+
+newtype DomainKnowledge = DomainKnowledge (Map Name ActionKnowledge)
+
+type Connection = (Int, Int, Literal Name)
+
+connections :: CondPred -> Set CondPred -> [(Connection, CondPred)]
+connections = undefined
+
+onSuccess :: ConditionalKnowledge -> Pattern -> ConditionalKnowledge
+onSuccess = undefined
+
+onFailure :: ConditionalKnowledge -> Pattern -> ConditionalKnowledge
+onFailure = undefined
+
+failed :: [PredicateSpec]
+       -> [Object]
+       -> ActionKnowledge
+       -> Transition
+       -> [Pattern]
+failed = undefined
+
+succeeded :: [PredicateSpec] -> [Object] -> Transition -> [Pattern]
+succeeded = undefined
+
+updateKnowledge :: PDDLDomain
+                -> PDDLProblem
+                -> DomainKnowledge
+                -> Transition
+                -> DomainKnowledge
+updateKnowledge = undefined
