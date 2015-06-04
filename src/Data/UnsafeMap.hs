ed3fcb1ceaa167d370d1a3aaab7be92d06c6256f
Søren Jacobsen <srnjcbsn@gmail.com>
2015-06-04 12:44:05 +0200

    Implemented unsafelookup in maps in its own module

---
+++ src/Data/UnsafeMap.hs
@@ -0,0 +1,9 @@
+module Data.UnsafeMap where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+
+-- | Performs normal lookup in a map, but raises an error with supplied message.
+unsLookup :: Ord k => String -> k -> Map k v -> v
+unsLookup err k m = fromMaybe (error err) (Map.lookup k m)
